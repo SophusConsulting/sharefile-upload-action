b01c5d7f4c57eb610d2671ecfd01d9881a3bf15b
Anthony Whitfield <anthonywhitfield5027@yahoo.com>
2024-06-13 15:37:18 -0700

    added tag input and concatenation

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const fs = require("fs");
 
 async function uploadToShareFile() {
 	try {
-
 		const clientID = core.getInput("client-id");
 		const clientSecret = core.getInput("client-secret");
 		const username = core.getInput("username");
@@ -14,14 +13,15 @@ async function uploadToShareFile() {
 		const filePath = core.getInput("path-to-file");
 		const fileName = core.getInput("file-name");
 		const folder = core.getInput("folder-to-upload");
+		const tag = core.getInput("tag");
 
 		const file = await fs.openAsBlob(filePath);
 
 		const token = await getToken(clientID, clientSecret, username, password);
 
-		const link = await getUploadLink(fileName, folder, token);
+		const uploadName = tag ? `${fileName}_${tag}` : fileName;
+		const link = await getUploadLink(uploadName, folder, token);
 		await uploadFile(file, fileName, link);
-
 	} catch (error) {
 		console.log(error);
 		core.setFailed(error.message);
